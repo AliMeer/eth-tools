fda02b2f96ae7b3ac84d81375cbbfc02c6364df2
ali@purple7.com <ali@purple7.com>
2019-04-02 23:47:31 +0100

    Sign message with private key

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Web3 from 'web3'
 import './App.css';
 
+var web3 = window.web3;
+
+
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      message: '',
+      signedMessage: ''
+    }
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleMessageChange = this.handleMessageChange.bind(this);
+  }
+  handleSubmit(event) {
+    event.preventDefault();
+    var web3 = window.web3;
+    if(typeof(web3)!=='undefined')  {
+      //web3 = new Web3(web3.currentProvider);
+      this.setState({signedMessage: 'Wallet detected'})
+      //create message packet to sign
+      let messageParams = [
+        {
+          type: 'string',
+          name: 'message',
+          value: this.state.message
+        }
+      ];
+      const fromAddress = web3.eth.accounts[0];
+      const method = 'eth_signTypedData';
+      const params = [messageParams, fromAddress]
+      web3.currentProvider.sendAsync({
+        method,
+        params,
+        fromAddress
+      }, (err, result) => {
+        if(err) console.log(err);
+        //this.setState({signedMessage: err});
+        if(result) this.setState({signedMessage: result.result});
+      }
+      )
+
+    }
+    else  {
+      this.setState({signedMessage: 'No Wallet detected in Browser'})
+    }
+
+
+  }
+  handleMessageChange(event)  {
+    event.preventDefault();
+    this.setState({message: event.target.value});
+    console.log(this.state.message)
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        
+        <form onSubmit={this.handleSubmit}>
+  <label>
+    Enter Message:
+    <input value={this.state.message} type="text" name="message" onChange={this.handleMessageChange} />
+  </label>
+  <input type="submit" value="Submit" />
+</form>
+
+      Signed Message: 
+      <textarea align="left" value={this.state.signedMessage} />
+  
       </div>
     );
   }
